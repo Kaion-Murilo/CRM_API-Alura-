3429dc199e34f77058228d561357e4b1b2986f54
Kaion Murilo <116471682+Kaion-Murilo@users.noreply.github.com>
2026-06-26 02:35:43 -0300

    Conclusão da primeira etapa.
    O que foi implementado:
    
    Criar o projeto base e o módulo responsável pelo cadastro de clientes.
    Definir o modelo de dados com os campos necessários.
    Garantir que as informações possam ser armazenadas e recuperadas pela API.
    Expor dois endpoints públicos: um para cadastrar clientes e outro para listá-los.
    Garantir que a data de criação seja preenchida automaticamente e que o email não se repita.

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from clientes.models import Client
 
-# Register your models here.
+class Clientes(admin.ModelAdmin):
+    list_display = ('id','name','email','phone','status','created_at','updated_at')
+    list_display_links = ('id','name',)
+    list_per_page = 20
+    search_fields = ('name',)
+        
+admin.site.register(Client,Clientes)
--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
-
-# Create your models here.
+class Client(models.Model):
+    StatusChoices = (
+        ('L','Lead'),
+        ('A','Ativo'),
+        ('I','Inativo'),
+    ) 
+    id         = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name       = models.CharField(max_length=255)
+    email      = models.EmailField(unique=True)
+    phone      = models.CharField(max_length=20, blank=True)
+    status     = models.CharField(max_length=20, choices=StatusChoices, default="I")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
